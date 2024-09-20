e83c105466703404df3d9ab888d4cd0aa0cf966a
something-natural <tsglow@naver.com>
2024-09-20 14:53:23 +0900

    feat: done selection sort function

--- script.js
+++ script.js
@@ -16,6 +16,23 @@ const bubbleSort = (array) => {
     return array;
 };
 
+const selectionSort = (array) => {
+    for (let i = 0 ; i < array.length ; i++){
+        let minNumIndex = i; // set min number's index to i
+        for (let j = i+1 ; j < array.length ; j++){                        
+            if ( array[j] < array[minNumIndex]){ 
+                minNumIndex = j; // change min number's index to j
+            }            
+        }
+        // using change numbers using index
+        const temp = array[i]        
+        array[i] = array[minNumIndex]
+        array[minNumIndex] = temp;
+    }
+    return array;
+}
+
+
 
 //function to render
 const renderResult = (array = []) => {
@@ -36,9 +53,10 @@ const sortInputArray = (e) => {
     e.preventDefault();    
     
     //get input value and turn to number array
-    const inputValues = [...document.getElementsByClassName("values-dropdown")].map((item) => Number(item.value))
+    const inputValues = [...document.getElementsByClassName("values-dropdown")].map((item) => Number(item.value));
 
-    const result = bubbleSort(inputValues)
+    //const result = bubbleSort(inputValues);
+    const result = selectionSort(inputValues)
     
     //render result
     renderResult(inputValues);
@@ -46,4 +64,4 @@ const sortInputArray = (e) => {
 
 
 //init
-sortBtn.addEventListener("click", sortInputArray)
+sortBtn.addEventListener("click", sortInputArray);
